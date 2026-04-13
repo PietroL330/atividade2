87d158058673cc49b93027bc6b18e4c7a1d9b040
PietroL330 <plessa330@gmail.com>
2026-04-13 15:07:39 -0300

    atividade valor ingresso

---
+++ atividade ingresso/atividade ingresso/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atividade_ingresso
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double vi, vid;
+
+            Console.WriteLine("insira o valor do ingresso:");
+            vi = double.Parse(Console.ReadLine());
+            if (vi > 200)
+            {
+                vid = vi * 0.90;
+                Console.WriteLine("o valor com desconto aplicado é " + vid + " Reais");
+            }
+            else
+            {
+                Console.WriteLine("não há desconto.");
+            }
+        }
+    }
+}
